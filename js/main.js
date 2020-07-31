142bfd0f2812af9ee54259bccdcc9423939de208
jim-clark <email@jim-clark.com>
2020-07-31 13:15:19 -0700

    Init

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 /*----- constants -----*/
-
+const WORDS = [
+  'DEVELOPER', 'ENGINEER', 'NODE', 'JAVASCRIPT',
+  'CODING', 'HTML', 'GUI', 'BOOLEAN',
+  'REACT', 'FUNCTION', 'COMPUTER SCIENCE',
+  'SEPARATION OF CONCERNS'
+];
+const MAX_WRONG = 6;
 
 /*----- app's state (variables) -----*/
-
+let secretWord;   // holds the randomly selected word
+let wrongLetters;   // array of strings
+let guess;   // string - that is initialized to same length as the secret
 
 /*----- cached element references -----*/
 
@@ -11,3 +19,16 @@
 
 
 /*----- functions -----*/
+init();
+
+function init() {
+  // Initialize all state and lastly call render()
+  wrongLetters = [];
+  // Get random index for secret word
+  const rndIdx = Math.floor(Math.random() * WORDS.length);
+  secretWord = WORDS[rndIdx];
+  guess = '';
+  for (let char of secretWord) {
+    guess += (char === ' ') ? ' ' : '_';
+  }
+}
